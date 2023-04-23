60cc32bd715070c694a1882ca225df84e21ed941
lyk990 <1477435414@qq.com>
2023-04-23 22:44:10 +0800

    transformIndexHtml

--- packages/vite/src/node/server/middlewares/indexHtml.ts
+++ packages/vite/src/node/server/middlewares/indexHtml.ts
@@ -1,3 +1,14 @@
 import { ViteDevServer } from "..";
+import type { Connect } from "dep-types/connect";
 
-export function indexHtmlMiddleware(server: ViteDevServer,) {}
+export function indexHtmlMiddleware(
+  server: ViteDevServer
+): Connect.NextHandleFunction {
+  return async function viteIndexHtmlMiddleware(req, res, next) {
+    console.log(req, "req");
+    console.log(res, "res");
+    console.log(next, "next");
+    const result = await server.transformIndexHtml(url, html, req.originalUrl);
+  };
+}
+
